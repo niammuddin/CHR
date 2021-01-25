d20b5629f805f75ddf608ebff196fbd0c5e5cfbd
niammuddin <niammuddin@gmail.com>
2021-01-25 09:56:53 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Update & Upgrade
+read -p "Are you want to Update this system [y/n] " update
+if [[ $update =~ ^[Yy]$ ]]
+	then
+		sudo apt-get update -y
+		read -p "Are you want to upgrade this system [y/n] " upgrade
+		if [[ $upgrade =~ ^[Yy]$ ]]
+		then 
+			sudo apt-get upgrade -y
+		fi
+
+fi  
+
+# Install modul
+read -p "Install wget, git, zip, unzip [y/n] " modul
+if [[ $modul =~ ^[Yy]$ ]]
+	then
+		sudo apt-get install -y wget git zip unzip
+fi
+
+mount -t tmpfs tmpfs /tmp/
+
+cd /tmp
+
+wget https://download.mikrotik.com/routeros/6.47.7/chr-6.47.7.img.zip
+
+unzip chr-6.47.7.img.zip
+
+if [ -e "/dev/vda" ]
+then
+dd if=chr-6.47.7.img of=/dev/vda bs=4M oflag=sync
+else
+dd if=chr-6.47.7.img of=/dev/sda bs=4M oflag=sync
+fi
+
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
+
+echo ">>> Finished Installing MIkrotik CHR <<<"
+
